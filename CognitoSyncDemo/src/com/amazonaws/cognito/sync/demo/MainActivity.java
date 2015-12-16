598947274a47585524a3d80ce6c26fc27e0774a6
chetanme <chetanme@usc.edu>
2015-12-15 22:32:54 -0800

    Adding null check before making a toast with user name

--- CognitoSyncDemo/src/com/amazonaws/cognito/sync/demo/MainActivity.java
+++ CognitoSyncDemo/src/com/amazonaws/cognito/sync/demo/MainActivity.java
@@ -478,7 +478,12 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(String response) {
             dialog.dismiss();
-            Toast.makeText(MainActivity.this, "Hello " + response, Toast.LENGTH_LONG).show();
+            if (response != null) {
+                Toast.makeText(MainActivity.this, "Hello " + response, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Unable to get user name from Facebook",
+                        Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
