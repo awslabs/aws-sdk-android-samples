9be846aad91585b2b7a85066ad7ecfb8c3d47f78
minbi <minbi@users.noreply.github.com>
2018-12-13 12:28:45 -0800

    Update iot web socket sample with mobile client (#277)

--- AndroidPubSubWebSocket/src/com/amazonaws/demo/androidpubsubwebsocket/PubSubActivity.java
+++ AndroidPubSubWebSocket/src/com/amazonaws/demo/androidpubsubwebsocket/PubSubActivity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
- *
- *    http://aws.amazon.com/apache2.0
- *
+ * <p>
+ * http://aws.amazon.com/apache2.0
+ * <p>
  * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
  * OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and
@@ -22,15 +22,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.io.UnsupportedEncodingException;
-import java.util.UUID;
 
-import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
-import com.amazonaws.regions.Regions;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 public class PubSubActivity extends Activity {
 
@@ -40,14 +43,7 @@ public class PubSubActivity extends Activity {
 
     // Customer specific IoT endpoint
     // AWS Iot CLI describe-endpoint call returns: XXXXXXXXXX.iot.<region>.amazonaws.com,
-    private static final String CUSTOMER_SPECIFIC_ENDPOINT = "CHANGE_ME";
-
-    // Cognito pool ID. For this app, pool needs to be unauthenticated pool with
-    // AWS IoT permissions.
-    private static final String COGNITO_POOL_ID = "CHANGE_ME";
-
-    // Region of AWS IoT
-    private static final Regions MY_REGION = Regions.US_EAST_1;
+    private static final String CUSTOMER_SPECIFIC_IOT_ENDPOINT = "REPLACE_ME";
 
     EditText txtSubscribe;
     EditText txtTopic;
@@ -58,40 +54,25 @@ public class PubSubActivity extends Activity {
     TextView tvStatus;
 
     Button btnConnect;
-    Button btnSubscribe;
-    Button btnPublish;
-    Button btnDisconnect;
 
     AWSIotMqttManager mqttManager;
     String clientId;
-
-    CognitoCachingCredentialsProvider credentialsProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtSubscribe = (EditText) findViewById(R.id.txtSubscribe);
-        txtTopic = (EditText) findViewById(R.id.txtTopic);
-        txtMessage = (EditText) findViewById(R.id.txtMessage);
+        txtSubscribe = findViewById(R.id.txtSubscribe);
+        txtTopic = findViewById(R.id.txtTopic);
+        txtMessage = findViewById(R.id.txtMessage);
 
-        tvLastMessage = (TextView) findViewById(R.id.tvLastMessage);
-        tvClientId = (TextView) findViewById(R.id.tvClientId);
-        tvStatus = (TextView) findViewById(R.id.tvStatus);
+        tvLastMessage = findViewById(R.id.tvLastMessage);
+        tvClientId = findViewById(R.id.tvClientId);
+        tvStatus = findViewById(R.id.tvStatus);
 
-        btnConnect = (Button) findViewById(R.id.btnConnect);
-        btnConnect.setOnClickListener(connectClick);
+        btnConnect = findViewById(R.id.btnConnect);
         btnConnect.setEnabled(false);
-
-        btnSubscribe = (Button) findViewById(R.id.btnSubscribe);
-        btnSubscribe.setOnClickListener(subscribeClick);
-
-        btnPublish = (Button) findViewById(R.id.btnPublish);
-        btnPublish.setOnClickListener(publishClick);
-
-        btnDisconnect = (Button) findViewById(R.id.btnDisconnect);
-        btnDisconnect.setOnClickListener(disconnectClick);
 
         // MQTT client IDs are required to be unique per AWS IoT account.
         // This UUID is "practically unique" but does not _guarantee_
@@ -99,141 +80,113 @@ public class PubSubActivity extends Activity {
         clientId = UUID.randomUUID().toString();
         tvClientId.setText(clientId);
 
-        // Initialize the AWS Cognito credentials provider
-        credentialsProvider = new CognitoCachingCredentialsProvider(
-                getApplicationContext(), // context
-                COGNITO_POOL_ID, // Identity Pool ID
-                MY_REGION // Region
+        // Initialize the credentials provider
+        final CountDownLatch latch = new CountDownLatch(1);
+        AWSMobileClient.getInstance().initialize(
+                getApplicationContext(),
+                new Callback<UserStateDetails>() {
+                    @Override
+                    public void onResult(UserStateDetails result) {
+                        latch.countDown();
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        latch.countDown();
+                        Log.e(LOG_TAG, "onError: ", e);
+                    }
+                }
         );
 
-        // MQTT Client
-        mqttManager = new AWSIotMqttManager(clientId, CUSTOMER_SPECIFIC_ENDPOINT);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        // The following block uses a Cognito credentials provider for authentication with AWS IoT.
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        btnConnect.setEnabled(true);
-                    }
-                });
-            }
-        }).start();
+        // MQTT Client
+        mqttManager = new AWSIotMqttManager(clientId, CUSTOMER_SPECIFIC_IOT_ENDPOINT);
+
+        // Enable button once all clients are ready
+        btnConnect.setEnabled(true);
     }
 
-    View.OnClickListener connectClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    public void connect(final View view) {
+        Log.d(LOG_TAG, "clientId = " + clientId);
 
-            Log.d(LOG_TAG, "clientId = " + clientId);
+        try {
+            mqttManager.connect(AWSMobileClient.getInstance(), new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(final AWSIotMqttClientStatus status,
+                                            final Throwable throwable) {
+                    Log.d(LOG_TAG, "Status = " + String.valueOf(status));
 
-            try {
-                mqttManager.connect(credentialsProvider, new AWSIotMqttClientStatusCallback() {
-                    @Override
-                    public void onStatusChanged(final AWSIotMqttClientStatus status,
-                            final Throwable throwable) {
-                        Log.d(LOG_TAG, "Status = " + String.valueOf(status));
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvStatus.setText(status.toString());
+                            if (throwable != null) {
+                                Log.e(LOG_TAG, "Connection error.", throwable);
+                            }
+                        }
+                    });
+                }
+            });
+        } catch (final Exception e) {
+            Log.e(LOG_TAG, "Connection error.", e);
+            tvStatus.setText("Error! " + e.getMessage());
+        }
+    }
 
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if (status == AWSIotMqttClientStatus.Connecting) {
-                                    tvStatus.setText("Connecting...");
+    public void subscribe(final View view) {
+        final String topic = txtSubscribe.getText().toString();
 
-                                } else if (status == AWSIotMqttClientStatus.Connected) {
-                                    tvStatus.setText("Connected");
+        Log.d(LOG_TAG, "topic = " + topic);
 
-                                } else if (status == AWSIotMqttClientStatus.Reconnecting) {
-                                    if (throwable != null) {
-                                        Log.e(LOG_TAG, "Connection error.", throwable);
+        try {
+            mqttManager.subscribeToTopic(topic, AWSIotMqttQos.QOS0,
+                    new AWSIotMqttNewMessageCallback() {
+                        @Override
+                        public void onMessageArrived(final String topic, final byte[] data) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        String message = new String(data, "UTF-8");
+                                        Log.d(LOG_TAG, "Message arrived:");
+                                        Log.d(LOG_TAG, "   Topic: " + topic);
+                                        Log.d(LOG_TAG, " Message: " + message);
+
+                                        tvLastMessage.setText(message);
+
+                                    } catch (UnsupportedEncodingException e) {
+                                        Log.e(LOG_TAG, "Message encoding error.", e);
                                     }
-                                    tvStatus.setText("Reconnecting");
-                                } else if (status == AWSIotMqttClientStatus.ConnectionLost) {
-                                    if (throwable != null) {
-                                        Log.e(LOG_TAG, "Connection error.", throwable);
-                                        throwable.printStackTrace();
-                                    }
-                                    tvStatus.setText("Disconnected");
-                                } else {
-                                    tvStatus.setText("Disconnected");
-
                                 }
-                            }
-                        });
-                    }
-                });
-            } catch (final Exception e) {
-                Log.e(LOG_TAG, "Connection error.", e);
-                tvStatus.setText("Error! " + e.getMessage());
-            }
+                            });
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Subscription error.", e);
         }
-    };
+    }
 
-    View.OnClickListener subscribeClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    public void publish(final View view) {
+        final String topic = txtTopic.getText().toString();
+        final String msg = txtMessage.getText().toString();
 
-            final String topic = txtSubscribe.getText().toString();
-
-            Log.d(LOG_TAG, "topic = " + topic);
-
-            try {
-                mqttManager.subscribeToTopic(topic, AWSIotMqttQos.QOS0,
-                        new AWSIotMqttNewMessageCallback() {
-                            @Override
-                            public void onMessageArrived(final String topic, final byte[] data) {
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        try {
-                                            String message = new String(data, "UTF-8");
-                                            Log.d(LOG_TAG, "Message arrived:");
-                                            Log.d(LOG_TAG, "   Topic: " + topic);
-                                            Log.d(LOG_TAG, " Message: " + message);
-
-                                            tvLastMessage.setText(message);
-
-                                        } catch (UnsupportedEncodingException e) {
-                                            Log.e(LOG_TAG, "Message encoding error.", e);
-                                        }
-                                    }
-                                });
-                            }
-                        });
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Subscription error.", e);
-            }
+        try {
+            mqttManager.publishString(msg, topic, AWSIotMqttQos.QOS0);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Publish error.", e);
         }
-    };
+    }
 
-    View.OnClickListener publishClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            final String topic = txtTopic.getText().toString();
-            final String msg = txtMessage.getText().toString();
-
-            try {
-                mqttManager.publishString(msg, topic, AWSIotMqttQos.QOS0);
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Publish error.", e);
-            }
-
+    public void disconnect(final View view) {
+        try {
+            mqttManager.disconnect();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Disconnect error.", e);
         }
-    };
-
-    View.OnClickListener disconnectClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-
-            try {
-                mqttManager.disconnect();
-            } catch (Exception e) {
-                Log.e(LOG_TAG, "Disconnect error.", e);
-            }
-
-        }
-    };
+    }
 }
