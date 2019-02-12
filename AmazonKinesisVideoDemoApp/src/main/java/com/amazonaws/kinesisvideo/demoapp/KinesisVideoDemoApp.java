952e3c6ec2409ec589f2c02251a57a6422c25b1b
Min Bi <min.bi.888@gmail.com>
2019-02-12 14:58:59 -0800

    [KinesisVideo] Add latch to ensure AWSMobileClient has initialized before usage

--- AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/KinesisVideoDemoApp.java
+++ AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/KinesisVideoDemoApp.java
@@ -9,6 +9,8 @@ import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.UserStateDetails;
 import com.amazonaws.regions.Regions;
 
+import java.util.concurrent.CountDownLatch;
+
 public class KinesisVideoDemoApp extends Application {
     public static final String TAG = KinesisVideoDemoApp.class.getSimpleName();
     public static Regions KINESIS_VIDEO_REGION = Regions.US_WEST_2;
@@ -21,17 +23,25 @@ public class KinesisVideoDemoApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        final CountDownLatch latch = new CountDownLatch(1);
         AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
             @Override
             public void onResult(UserStateDetails result) {
                 Log.d(TAG, "onResult: user state: " + result.getUserState());
+                latch.countDown();
             }
 
             @Override
             public void onError(Exception e) {
                 Log.e(TAG, "onError: Initialization error of the mobile client", e);
+                latch.countDown();
             }
         });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
