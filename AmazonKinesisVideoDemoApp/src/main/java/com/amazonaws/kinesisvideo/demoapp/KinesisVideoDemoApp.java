2723ae07daf0712f12ea36172ce6ff4dba7ee5d3
hu <zhiyua@amazon.com>
2020-01-27 18:10:50 -0800

    Revert "Use iot-kvs credentials provider"
    This reverts commit 34a85ae60c87768135c11b89e12d2c17a54f45df.

--- AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/KinesisVideoDemoApp.java
+++ AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/KinesisVideoDemoApp.java
@@ -3,34 +3,20 @@ package com.amazonaws.kinesisvideo.demoapp;
 import android.app.Application;
 import android.util.Log;
 
-import com.amazonaws.kinesisvideo.auth.KinesisVideoCredentialsProvider;
-import com.amazonaws.kinesisvideo.common.logging.LogLevel;
-import com.amazonaws.kinesisvideo.common.logging.OutputChannel;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobile.client.Callback;
 import com.amazonaws.mobile.client.UserStateDetails;
-import com.amazonaws.mobileconnectors.kinesisvideo.auth.AwsIoTKinesisVideoCredentialsProvider;
-import com.amazonaws.mobileconnectors.kinesisvideo.util.AndroidLogOutputChannel;
 import com.amazonaws.regions.Regions;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 import java.util.concurrent.CountDownLatch;
 
 public class KinesisVideoDemoApp extends Application {
     public static final String TAG = KinesisVideoDemoApp.class.getSimpleName();
     public static Regions KINESIS_VIDEO_REGION = Regions.US_WEST_2;
 
-    public static KinesisVideoCredentialsProvider getCredentialsProvider() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException {
-        final OutputChannel outputChannel = new AndroidLogOutputChannel();
-        final com.amazonaws.kinesisvideo.common.logging.Log log =
-                new com.amazonaws.kinesisvideo.common.logging.Log(outputChannel, LogLevel.VERBOSE, TAG);
-        return new AwsIoTKinesisVideoCredentialsProvider("REPLACE_ME_awsIotAuthUrl", "REPLACE_ME_keyStoreLocation",
-                "REPLACE_ME_keyStorePassword", log);
+    public static AWSCredentialsProvider getCredentialsProvider() {
+        return AWSMobileClient.getInstance();
     }
 
     @Override
