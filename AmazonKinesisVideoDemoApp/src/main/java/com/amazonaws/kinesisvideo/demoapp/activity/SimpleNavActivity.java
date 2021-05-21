1c009fcfe62a252fab9aa84d23321acebb0208b8
Kho Yee, Choy <choyk@amazon.co.jp>
2021-05-21 19:47:51 +0900

    migrate to Android X and updated AWS SDK for Android

--- AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/activity/SimpleNavActivity.java
+++ AmazonKinesisVideoDemoApp/src/main/java/com/amazonaws/kinesisvideo/demoapp/activity/SimpleNavActivity.java
@@ -1,14 +1,14 @@
 package com.amazonaws.kinesisvideo.demoapp.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
